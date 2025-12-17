b1eade7a26da08a7315cbe06aa5e56be8125351e
sumansauravmay <sumansauravmay12345@gmail.com>
2025-12-17 13:42:12 +0530

    output based question

--- Output/index.js
+++ Output/index.js
@@ -1,67 +1,65 @@
-
-
-
-
-// 4.
-
-// const a = [1, 2, 3];
-// a[7] = 6;
-// console.log(a);
-
+/*
+const a = [1, 2, 3];
+a[7] = 6;
+console.log(a);
 // output: [ 1, 2, 3, <4 empty items>, 6 ]
+*/
 
-// 5.
-
-// const obj={
-//     a:1,
-//     b:2
-// }
-
-// obj["k"]=5;
-// console.log(obj)
-
+/*
+const obj={
+    a:1,
+    b:2
+}
+obj["k"]=5;
+console.log(obj)
 // output: { a: 1, b: 2, k: 5 }
+*/
 
-// 6.
-
-// const obj={
-//     a:1,
-//     b:2
-// }
-
-// obj[k]=5;
-// console.log(obj)
-
+/*
+const obj={
+    a:1,
+    b:2
+}
+obj[k]=5;
+console.log(obj)
 // output: error(k is not defined)
+*/
 
-// 7.
+/*
+(()=>{
+    let a=b=10;
+})();
+ console.log(b);//10
+console.log(a);//a is not defined
+*/
 
-// (()=>{
-//     let a=b=10;
-// })();
+/*
+function name2(){
+    let a=10;
+}
+name2();
+console.log(a)//a is not defined
+*/
 
-// console.log(b);//10
-// console.log(a);//a is not defined
+/*
+console.log(typeof undefined); //undefined
+console.log(typeof NaN); //number
+console.log(typeof null); //object
+console.log([] == []); //false
+console.log([] === []); //false
+console.log({} == {}); //false
+console.log({} === {}); //false
 
-// 8.
+console.log(undefined == null); //true
+console.log(undefined === null); //false
+console.log(NaN == NaN); //false
+console.log(NaN === NaN); //false
+*/
 
-// function name2(){
-//     let a=10;
-// }
 
-// name2();
-// console.log(a)//a is not defined
 
-// 9.
-// console.log(typeof undefined); //undefined
-// console.log(typeof NaN); //number
-// console.log(typeof null); //object
-// console.log([] == []); //false
-// console.log([] === []); //false
-// console.log({} == {}); //false
-// console.log({} === {}); //false
 
-// console.log(undefined == null); //true
-// console.log(undefined === null); //false
-// console.log(NaN == NaN);//false
-// console.log(NaN === NaN);//false
+
+
+
+
