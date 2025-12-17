834021cde902db6cc898b2fecf937d03e3027144
sumansauravmay <sumansauravmay12345@gmail.com>
2025-12-17 15:58:41 +0530

    Output data

--- Output/index.js
+++ Output/index.js
@@ -398,7 +398,6 @@ console.log(array.length);
 // Output: 5( "delete" removes the element but does not reindex the array or change its length. It leaves undefined holes in the array)
 */
 
-
 /*
 let x = ["a", "b", "c"];
 let y = ["a", "b", "c"];
@@ -408,8 +407,76 @@ console.log(z == y); // true(Same memory location)
 console.log(z == x); // false(Different memory location)
 */
 
+/*
+let x; 
+console.log(x);
+x = 20;
+console.log(x);
+x = "John";
+console.log(x);
+// Output: undefined, 20, John
+*/
+
+/*
+let text;
+switch (1) {
+  case 0:
+    text = "This is zero";
+    break;
+  case 1:
+    text = "This is one";
+  case 2:
+    text = "This is two";
+    break;
+  default:
+    text = "No matches found!";
+}
+console.log(text);
+
+// Output: This is two(switch(1) means JS looks for a case that matches 1. It finds case 1 & sets text as "This is one". But there is no break, so it keeps going (falls through) into case 2 & overwrites the value.)
+*/
+
+/*
+const user = {
+    name: 'Aman Bhoria!',
+    logMessage() {
+        console.log(this.name); // What is logged? 
+    }
+}; 
+setTimeout(user.logMessage, 1000);
+// Output: Undefined(We've passed the reference in setTimeout not the actual function so as a result it doesn't have the user's context while executing. To get the name we've to pass a callback like: setTimeout(() => user.logMessage(), 1000);)
+*/
+
+/*
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+const finalObj = Object.assign({}, obj1, obj2);
+console.log(finalObj);
+// Output: { a: 1, b: 3, c: 4 }(When two or more objects are merged: If keys conflict (i.e., the same key exists in multiple objects), the value from the later object overwrites the earlier one.)
+*/
+
+/*
+let a = {};
+let b = { key: "abc" };
+let c = { key: "efg" };
+
+a[b] = 111;
+a[c] = 222;
+console.log(a[b]);
+// Output: 222(In JavaScript, using an object as a key in a normal object turns it into a string. That string is usually "[object Object]". So, two different objects like b and c become the same key -> a[b] = 111 & a[c] = 222 becomes a["[object Object]"] = 111 & a["[object Object]"] = 222. Hence, the second value (222) replaces the first one.)
+*/
 
 
 
+/*
+function printName(firstName, lastName) {
+    firstName = "Aman";
+    lastName = "Bhoria";
+    // return firstName + " " + lastName;
+    return arguments[0] + " " + arguments[1];
+}
 
-
+let name = printName("John", "Doe");
+console.log(name)
+// Output: Aman Bhoria
+*/
