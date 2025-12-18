dda9be1097553ba6da992675a9d410de34f7a722
sumansauravmay <sumansauravmay12345@gmail.com>
2025-12-18 18:48:49 +0530

    focus on input

--- React/useref/src/App.jsx
+++ React/useref/src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useRef } from 'react';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
- const ref=useRef(null);
-
- const submitBtn=()=>{
-  ref.current.focus();
-  console.log(ref.current.value);
- }
+  const ref = useRef(null);
+  const submitBtn = () => {
+    ref.current.focus();
+    console.log(ref.current.value);
+  };
 
   return (
     <>
-   <input ref={ref} type="text"/>
-   <button onClick={submitBtn}>Focus</button>
+      <input ref={ref} type="text" />
+      <button onClick={submitBtn}>Focus</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
