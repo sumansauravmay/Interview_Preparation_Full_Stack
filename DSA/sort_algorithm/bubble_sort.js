d84d69ab57057eb9a3ec33de6ccfaf298b4eeebf
sumansauravmay <sumansauravmay12345@gmail.com>
2025-12-18 19:10:54 +0530

    bubble sort

---
+++ DSA/sort_algorithm/bubble_sort.js
@@ -0,0 +1,46 @@
+let arr = [7, 3, 4, 1, 2, 9, 5, 6, 8];
+
+/*
+for (let i = 0; i < arr.length; i++) {
+  let isFlagged = false;
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      isFlagged = true;
+    }
+  }
+  if (!isFlagged) break;
+}
+console.log(arr);
+// Time complexity: O(n)
+*/
+
+/*
+let isFlagged = false;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      isFlagged = true;
+    }
+  }
+  if (!isFlagged) break;
+}
+console.log(arr);
+// Time complexity: O(n**2)
+*/
+
+/*
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+     let temp=arr[j];
+     arr[j]=arr[j+1];
+     arr[j+1]=temp
+    }
+  }
+  
+}
+console.log(arr);
+// Time complexity: O(n**2)
+*/
