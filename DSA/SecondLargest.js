85eba4f162441a85d77049218ae11c7ed0034abc
sumansauravmay <sumansauravmay12345@gmail.com>
2026-01-05 15:21:09 +0530

    second largest

--- DSA/SecondLargest.js
+++ DSA/SecondLargest.js
@@ -12,7 +12,7 @@ for (let i = 0; i < arr.length; i++) {
   if (arr[i] > largest) {
     secondLargest = largest;
     largest = arr[i];
-  } else if (arr[i] > secondLargest) {
+  } else if (arr[i] > secondLargest && arr[i] != largest) {
     secondLargest = arr[i];
   }
 }
