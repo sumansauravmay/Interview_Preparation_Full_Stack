72c2df369bb3bf9f70ca642ce028a3d825d2658a
sumansauravmay <sumansauravmay12345@gmail.com>
2026-01-13 13:09:11 +0530

    reverse star pattern

--- DSA/starPattern/5.js
+++ DSA/starPattern/5.js
@@ -9,9 +9,11 @@ let n = 5;
 
 for (let i = 0; i < n; i++) {
   let row = "";
+  // This is for adding space
   for (let j = 0; j < n - i - 1; j++) {
     row = row + " ";
   }
+  // this is for adding stars
   for (let k = 0; k < i + 1; k++) {
     row += "*";
   }
