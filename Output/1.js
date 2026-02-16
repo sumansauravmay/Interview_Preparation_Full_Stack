e33b52e55d847bdcf76e48b7399cd9cab77acaa1
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-16 13:35:13 +0530

    set time out problem

---
+++ Output/1.js
@@ -0,0 +1,18 @@
+for (var i = 0; i < 5; i++) {
+    setTimeout(() => {
+        console.log(i)
+    }, i)
+}
+
+
+
+
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(() => {
+        console.log(i)
+    }, i)
+}
+
+
+
