b19173ae7686750a47c670cad2daa48f1e5a769b
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-16 13:50:17 +0530

    sorting using bubble sort

--- DSA/sort_algorithm/bubble_sort.js
+++ DSA/sort_algorithm/bubble_sort.js
@@ -1,6 +1,6 @@
 let arr = [7, 3, 4, 1, 2, 9, 5, 6, 8];
 
-/*
+
 for (let i = 0; i < arr.length; i++) {
   let isFlagged = false;
   for (let j = 0; j < arr.length - 1 - i; j++) {
@@ -13,7 +13,7 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log(arr);
 // Time complexity: O(n)
-*/
+
 
 /*
 let isFlagged = false;
@@ -44,3 +44,11 @@ for (let i = 0; i < arr.length; i++) {
 console.log(arr);
 // Time complexity: O(n**2)
 */
+
+/*
+output: [
+  1, 2, 3, 4, 5,
+  6, 7, 8, 9
+]
+*/
+
