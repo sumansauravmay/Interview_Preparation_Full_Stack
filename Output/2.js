ae6bb4c5ce6ebd21fa663e50186a63280f3c44f7
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-16 13:39:06 +0530

    number type

---
+++ Output/2.js
@@ -0,0 +1,13 @@
+if(0.2+0.1===0.3){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+/*
+output: false because
+0.1 and 0.2 can't represent exactly in binary
+0.1=0.1000000000000023
+0.2=0.2000000023444302
+sum=0.3.000000........
+*/
