39d2f0f6ae4a1ff26686ea993b9fc584402c23df
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-16 13:42:07 +0530

    update value using const

---
+++ Output/3.js
@@ -0,0 +1,18 @@
+const myname={
+    name:"Suman Saurav"
+}
+myname.name="Ram"
+
+console.log(myname)
+
+
+const arr=["apple", "Orange", "Guava"]
+arr[0]="veg"
+console.log(arr)
+
+
+/*
+output:
+{ name: 'Ram' }
+[ 'veg', 'Orange', 'Guava' ]
+*/
