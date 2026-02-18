d21088c6dd44946d857612fbcd1fd012f0c4d9f2
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-18 13:40:41 +0530

    object add key

---
+++ Output/7.js
@@ -0,0 +1,17 @@
+const obj1 = {
+    a: 1,
+    b: 2
+}
+obj1["k"] = 5;
+console.log(obj1)
+// output: { a: 1, b: 2, k: 5 }
+
+
+
+const obj2 = {
+    a: 1,
+    b: 2
+}
+obj2[k] = 5;
+console.log(obj2)
+// output: error(k is not defined)
