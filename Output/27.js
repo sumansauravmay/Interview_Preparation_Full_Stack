78105ba6a8a394a85fa6de131837ea34e97ff565
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-19 17:38:16 +0530

    function output using async and without async

--- Output/27.js
+++ Output/27.js
@@ -1,4 +1,5 @@
 
+/*
 async function test(){
 console.log(1)
 await Promise.resolve()
@@ -7,7 +8,7 @@ console.log(2)
 console.log(3)
 test()
 console.log(4)
-
+*/
 
 /*
 output: 
@@ -18,4 +19,25 @@ output:
 
 This is due to event loop. promise will go in the Macro task
 
+*/
+
+
+
+function test() {
+    console.log(1)
+    Promise.resolve()
+    console.log(2)
+}
+console.log(3)
+test()
+console.log(4)
+
+
+
+/*
+Output:
+3
+1
+2
+4
 */
