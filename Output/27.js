bc39b0635a2d26a59ccc946fd9be0ecbcd3c607f
sumansauravmay <sumansauravmay12345@gmail.com>
2026-02-19 14:25:37 +0530

    event loop output based problem

---
+++ Output/27.js
@@ -0,0 +1,21 @@
+
+async function test(){
+console.log(1)
+await Promise.resolve()
+console.log(2)
+}
+console.log(3)
+test()
+console.log(4)
+
+
+/*
+output: 
+3
+1
+4
+2
+
+This is due to event loop. promise will go in the Macro task
+
+*/
